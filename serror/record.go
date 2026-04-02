e8a8e0ee071bdeabb512ba4cfc194e56cc6b1a29
Nishad Wadwekar <nishad.wadwekar@gmail.com>
2026-04-02 20:03:34 +0530

    structured errors: supports slog

---
+++ serror/record.go
@@ -0,0 +1,54 @@
+package serror
+
+import (
+	"log/slog"
+	"strings"
+)
+
+const (
+	slogkeyErrorPrefix = "error."
+	slogkeyErrorTrait  = slogkeyErrorPrefix + "trait"
+	slogkeyErrorCode   = slogkeyErrorPrefix + "code"
+	slogkeyErrorTrace  = slogkeyErrorPrefix + "trace"
+	slogkeyErrorCause  = slogkeyErrorPrefix + "cause"
+)
+
+type ErrorRecord struct {
+	Trait   ErrorTrait
+	Trace   []string
+	Err     error
+	Context map[string]any
+}
+
+func (e *ErrorRecord) Error() string {
+	if e.Err == nil {
+		panic(PanicNilError)
+	}
+	return e.Err.Error()
+}
+
+func (e *ErrorRecord) Attrs() []slog.Attr {
+	attrs := []slog.Attr{}
+
+	if e.Err == nil {
+		panic(PanicNilError)
+	}
+
+	if e.Trait.Trait != "" {
+		attrs = append(attrs, slog.String(slogkeyErrorTrait, e.Trait.Trait))
+		attrs = append(attrs, slog.Int(slogkeyErrorCode, e.Trait.Code))
+	}
+
+	attrs = append(attrs, slog.String(slogkeyErrorTrace, strings.Join(e.Trace, " -> ")))
+	attrs = append(attrs, slog.String(slogkeyErrorCause, e.Err.Error()))
+
+	for k, v := range e.Context {
+		attrs = append(attrs, slog.Any(slogkeyErrorPrefix+k, v))
+	}
+
+	return attrs
+}
+
+func (e *ErrorRecord) LogValue() slog.Value {
+	return slog.GroupValue(e.Attrs()...)
+}
